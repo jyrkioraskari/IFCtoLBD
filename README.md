b56eb8b29ffcfec3ea4bddb3e126649e8d1e203d
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2018-02-09 19:04:59 +0100

    Maven installation for command line uaage

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ Resource Description Framework (RDF) triples that follow the small ontologies de
 (https://github.com/w3c-lbd-cg/).
 
 The precompiled desktop application, IFCtoLBD-Desktop.jar, is available at this page. It is a runnable JAR-file. It is recommended to run the JAR using Java JDK version 8 that can be downloaded from http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html .  If the Java installation is fine, the file can be run by clicking it. If not, try the following command at the command line: "java -jar IFCtoLBD-Desktop.jar".
+
+The converter can be compiled using maven and Java JDK (the above link). Maven can be downloaded from the site here: https://maven.apache.org/download.cgi.
+
+The following steps are needed:
+1. Make sure that the JAVA_HOME environment variable point to the JAVA JDK directory. JRE is not enough.
+2. cd converter
+3. mvn clean 
+4. mvn install
+
+The created package will be at the target subdirectory.
+You can rename the ifc2lbd-1.08-jar-with-dependencies.jar file into name IFCtoRDF.jar
+
+An example commaand line usage of the program is:
+java -jar IFCtoRDF.jar Duplex_A.ifc http://uribase  out.ttl
+
---
+++ converter/src/main/java/fi/ni/rdf/Namespace.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2016 Pieter Pauwels, Ghent University; Jyrki Oraskari, Aalto University; Lewis John McGibbney, Apache
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fi.ni.rdf;
+
+public class Namespace {
+    public static final String RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+    public static final String RDFS = "http://www.w3.org/2000/01/rdf-schema#";
+    public static final String OWL = "http://www.w3.org/2002/07/owl#";
+    public static final String XSD = "http://www.w3.org/2001/XMLSchema#";
+    public static String IFC = "http://ifcowl.openbimstandards.org/";
+    public static final String DCE = "http://purl.org/dc/elements/1.1/";
+    public static final String VANN = "http://purl.org/vocab/vann/";
+    public static final String CC = "http://creativecommons.org/ns#";
+    public static final String LIST = "https://w3id.org/list#";
+    public static final String EXPRESS = "https://w3id.org/express#";
+    public static final String SIMPLEBIM = "http://ifcowl.openbimstandards.org/SimpleBIM";
+}
