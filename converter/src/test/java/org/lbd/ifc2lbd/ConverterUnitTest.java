da6577161960489aa5d753ae431064f2a8c444ce
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2020-08-27 17:28:25 +0200

    SHACL JUnit test for the LBD output

--- converter/src/test/java/org/lbd/ifc2lbd/ConverterUnitTest.java
+++ converter/src/test/java/org/lbd/ifc2lbd/ConverterUnitTest.java
@@ -5,7 +5,13 @@ import static org.junit.jupiter.api.Assertions.fail;
 import java.io.File;
 import java.net.URL;
 
+import org.apache.jena.graph.Graph;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.shacl.ShaclValidator;
+import org.apache.jena.shacl.Shapes;
+import org.apache.jena.shacl.ValidationReport;
+import org.apache.jena.shacl.lib.ShLib;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,10 +23,10 @@ public class ConverterUnitTest {
 		URL file_url = ClassLoader.getSystemResource("Duplex.ifc");
 		try {
 			File file = new File(file_url.toURI());
-			if(file==null || !file.exists())
+			if (file == null || !file.exists())
 				fail("Test data not found/available");
 		} catch (Exception e) {
-			fail("Test data not found/available: "+e.getMessage());
+			fail("Test data not found/available: " + e.getMessage());
 		}
 	}
 
@@ -30,51 +36,80 @@ public class ConverterUnitTest {
 		URL file_url = ClassLoader.getSystemResource("Duplex.ifc");
 		try {
 			File ifc_file = new File(file_url.toURI());
-			File temp_file=File.createTempFile("ifc2lbd", "test.ttl");
-			new IFCtoLBDConverter(ifc_file.getAbsolutePath(), "https://dot.dc.rwth-aachen.de/IFCtoLBDset#", temp_file.getAbsolutePath(),
-					0, true, false, true, false, false, false);
+			File temp_file = File.createTempFile("ifc2lbd", "test.ttl");
+			new IFCtoLBDConverter(ifc_file.getAbsolutePath(), "https://dot.dc.rwth-aachen.de/IFCtoLBDset#",
+					temp_file.getAbsolutePath(), 0, true, false, true, false, false, false);
 		} catch (Exception e) {
-			fail("Conversion has an error: "+e.getMessage());
+			fail("Conversion had an error: " + e.getMessage());
 		}
 	}
-	
-	@DisplayName("Test conversion case 1")
+
+	@DisplayName("Test conversion test case 1")
 	@Test
 	public void testConversion1() {
 		URL file_url = ClassLoader.getSystemResource("Duplex.ifc");
 		try {
 			File ifc_file = new File(file_url.toURI());
-			File temp_file=File.createTempFile("ifc2lbd", "test.ttl");
-			IFCtoLBDConverter c1nb=new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", false,1);
-			c1nb.convert(ifc_file.getAbsolutePath(),temp_file.getAbsolutePath()); 
-			Model m1nb=c1nb.convert(ifc_file.getAbsolutePath());
+			File temp_file = File.createTempFile("ifc2lbd", "test.ttl");
+			IFCtoLBDConverter c1nb = new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", false, 1);
+			c1nb.convert(ifc_file.getAbsolutePath(), temp_file.getAbsolutePath());
+			Model m1nb = c1nb.convert(ifc_file.getAbsolutePath());
 
-			IFCtoLBDConverter c1wb=new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", true,1);
-			c1wb.convert(ifc_file.getAbsolutePath(),temp_file.getAbsolutePath()); 
-			Model m1wb=c1wb.convert(ifc_file.getAbsolutePath());
+			IFCtoLBDConverter c1wb = new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", true, 1);
+			c1wb.convert(ifc_file.getAbsolutePath(), temp_file.getAbsolutePath());
+			Model m1wb = c1wb.convert(ifc_file.getAbsolutePath());
 
-			
-			IFCtoLBDConverter c2nb=new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", false,2);
-			c2nb.convert(ifc_file.getAbsolutePath(),temp_file.getAbsolutePath()); 
-			Model m2nb=c2nb.convert(ifc_file.getAbsolutePath());
+			IFCtoLBDConverter c2nb = new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", false, 2);
+			c2nb.convert(ifc_file.getAbsolutePath(), temp_file.getAbsolutePath());
+			Model m2nb = c2nb.convert(ifc_file.getAbsolutePath());
 
-			IFCtoLBDConverter c2wb=new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", true,2);
-			c2wb.convert(ifc_file.getAbsolutePath(),temp_file.getAbsolutePath()); 
-			Model m2wb=c2wb.convert(ifc_file.getAbsolutePath());
+			IFCtoLBDConverter c2wb = new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", true, 2);
+			c2wb.convert(ifc_file.getAbsolutePath(), temp_file.getAbsolutePath());
+			Model m2wb = c2wb.convert(ifc_file.getAbsolutePath());
 
-			
-			IFCtoLBDConverter c3nb=new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", false,3);
-			c3nb.convert(ifc_file.getAbsolutePath(),temp_file.getAbsolutePath()); 
-			Model m3nb=c3nb.convert(ifc_file.getAbsolutePath());
+			IFCtoLBDConverter c3nb = new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", false, 3);
+			c3nb.convert(ifc_file.getAbsolutePath(), temp_file.getAbsolutePath());
+			Model m3nb = c3nb.convert(ifc_file.getAbsolutePath());
 
+			IFCtoLBDConverter c3wb = new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", true, 3);
+			c3wb.convert(ifc_file.getAbsolutePath(), temp_file.getAbsolutePath());
+			Model m3wb = c3wb.convert(ifc_file.getAbsolutePath());
 
-			IFCtoLBDConverter c3wb=new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", true,3);
-			c3wb.convert(ifc_file.getAbsolutePath(),temp_file.getAbsolutePath()); 
-			Model m3wb=c3wb.convert(ifc_file.getAbsolutePath());
-
-			
 		} catch (Exception e) {
-			fail("Conversion has an error: "+e.getMessage());
+			fail("Conversion set 1 had an error: " + e.getMessage());
 		}
 	}
+
+	@DisplayName("Test conversion test case Level 1")
+	@Test
+	public void testConversionLevel1() {
+		URL ifc_file_url = ClassLoader.getSystemResource("Duplex.ifc");
+		URL rule_file_url = ClassLoader.getSystemResource("SHACL_ruleset1.ttl");
+		try {
+			File ifc_file = new File(ifc_file_url.toURI());
+			IFCtoLBDConverter c1nb = new IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset#", false, 1);
+			Model m1nb = c1nb.convert(ifc_file.getAbsolutePath());
+			Graph graph_m1nb = m1nb.getGraph();
+
+			File rule1_file = new File(rule_file_url.toURI());
+			Graph shapesGraph = RDFDataMgr.loadGraph(rule1_file.getAbsolutePath());
+			Shapes shapes = Shapes.parse(shapesGraph);
+
+			ValidationReport report = ShaclValidator.get().validate(shapes, graph_m1nb);
+			if(!report.conforms())
+			{
+			  System.out.println("false");
+			  fail("Conversion output does not conform rule1");					
+			  ShLib.printReport(report);
+			  report.getModel().write(System.out);
+			}
+			else
+				System.out.println("Actually ok");
+		} catch (Exception e) {
+			System.out.println("ERROR");
+			e.printStackTrace();
+			fail("Conversion set 1 had an error: " + e.getMessage());
+		}
+	}
+
 }
