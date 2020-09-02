7ff1f4725e26293a525fc5edd90acd0e1e3d0370
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2020-09-02 18:32:09 +0200

    clean-up

--- IFCtoLBD_Python/IFCtoLBD.py
+++ IFCtoLBD_Python/IFCtoLBD.py
@@ -12,8 +12,6 @@ jpype.startJVM(classpath = ['jars/*'])
 
 from org.linkedbuildingdata.ifc2lbd import IFCtoLBDConverter
 
-jpype.java.lang.System.out.println("This was printed via System.out.println.")
-
 lbdconverter = IFCtoLBDConverter("https://dot.dc.rwth-aachen.de/IFCtoLBDset",  3)
 
 model=lbdconverter.convert("Duplex_A_20110505.ifc");
