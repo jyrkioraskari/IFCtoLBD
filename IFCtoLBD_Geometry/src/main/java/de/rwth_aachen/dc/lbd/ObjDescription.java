6385908cacc4ccab40bc6b3486a425b55cf3ce0a
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2023-02-13 11:31:33 +0100

    Geometry fixes

--- IFCtoLBD_Geometry/src/main/java/de/rwth_aachen/dc/lbd/ObjDescription.java
+++ IFCtoLBD_Geometry/src/main/java/de/rwth_aachen/dc/lbd/ObjDescription.java
@@ -56,9 +56,12 @@ public class ObjDescription {
     	
 		for(ImmutableTriple<Integer, Integer, Integer> f:this.faces)
     		sb.append("f "+f.left+" "+f.middle+" "+f.right+"\n");
+		String content=sb.toString();
+		//System.out.println("content"+content);
+		//System.out.println("org "+content.length());
+		String encodedString = Base64.getEncoder().encodeToString(content.getBytes());
 		
-		//String encodedString = Base64.getEncoder().encodeToString(sb.toString().getBytes());
-        //return encodedString;
-		return sb.toString();
+		//System.out.println("encoded "+encodedString.length());
+        return encodedString;
     }
 }
--- IFCtoLBD_Geometry/src/main/java/de/rwth_aachen/dc/lbd/obj/IFCtoObj.java
+++ IFCtoLBD_Geometry/src/main/java/de/rwth_aachen/dc/lbd/obj/IFCtoObj.java
@@ -83,7 +83,9 @@ public class IFCtoObj {
 				}
 				
 				ByteBuffer ver = geometry.getVertices().order(ByteOrder.nativeOrder());
-				for (int i = 0; i < geometry.getNrVertices() / 3; i++) {
+				
+				while (ver.hasRemaining())
+				{
 					Point3d p = processVertex(tranformationMatrix, ver);
 					obj_desc.addVertex(p);
 				}
@@ -93,11 +95,9 @@ public class IFCtoObj {
 					obj_desc.addFace(f);
 
 				}*/
-				
-				System.out.println("indices: "+geometry.getNrIndices());
-				System.out.println("indices: "+geometry.getIndices().limit());
 				ByteBuffer inx = geometry.getIndices().order(ByteOrder.nativeOrder());
-				for (int i = 0; i < geometry.getNrIndices()/3;  i++) {
+               											
+				while (inx.hasRemaining()) {
 					ImmutableTriple<Integer, Integer, Integer> f = processSurface(inx);
 					obj_desc.addFace(f);
 				}
