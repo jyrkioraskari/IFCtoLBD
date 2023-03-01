3d91e8342fbed86e78c4be89e146b37fd2fdf325
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2023-03-01 12:43:36 +0100

    Re org

--- IFCtoLBD_Desktop/src/main/java/org/linkedbuildingdata/ifc2lbd/desktop/Main.java
+++ IFCtoLBD_Desktop/src/main/java/org/linkedbuildingdata/ifc2lbd/desktop/Main.java
@@ -3,7 +3,6 @@ package org.linkedbuildingdata.ifc2lbd.desktop;
 public class Main {
 
     public static void main(String[] args) {
-    	//System.out.println("The unnamed module is fine.");
     	//JenaSystem.init();
     	IFCtoLBD_Desktop.main(args);
     }
