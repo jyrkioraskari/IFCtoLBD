d8108ba33d950f5d13c1f53c134e19aaefd1e849
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2023-03-01 12:50:54 +0100

    Java 15 style

---
+++ IFCtoLBD_Desktop/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module io.github.jyrkioraskari.IFCtoLBD.Desktop {
+	requires transitive io.github.jyrkioraskari.IFCtoLBD;
+	requires javafx.fxml;
+	requires javafx.controls;
+    requires org.controlsfx.controls;
+    requires com.google.common;
+    requires java.logging;
+    requires java.prefs;    
+    requires java.desktop;
+
+    exports org.linkedbuildingdata.ifc2lbd.desktop;
+    opens org.linkedbuildingdata.ifc2lbd.desktop;
+}
