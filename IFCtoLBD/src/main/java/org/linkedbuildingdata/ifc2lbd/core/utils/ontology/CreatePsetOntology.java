6da4a97e621eec5b23c6942f599e702fddb43ee9
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2024-01-05 14:16:44 +0100

    Refactoring

---
+++ IFCtoLBD/src/main/java/org/linkedbuildingdata/ifc2lbd/core/utils/ontology/CreatePsetOntology.java
@@ -0,0 +1,15 @@
+package org.linkedbuildingdata.ifc2lbd.core.utils.ontology;
+
+public class CreatePsetOntology {
+
+	public CreatePsetOntology() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new CreatePsetOntology();
+
+	}
+
+}
+ 
