b4acf930a96d0a215e0413cb3790b383d9ac8413
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2024-01-09 18:36:52 +0100

    Python update

--- IFCtoLBD_Python/IFCtoLBD_list_subjects_of_triples.py
+++ IFCtoLBD_Python/IFCtoLBD_list_subjects_of_triples.py
@@ -19,11 +19,17 @@ model=lbdconverter.convert("Duplex_A_20110505.ifc");
 statements=model.listStatements();
 
 
-# List all RDF subjects of the statements in the LBD Model
+# List all suvjects of the statements in the LBD Model
 while statements.hasNext() :
         triple=statements.next()
         subject=triple.getSubject().toString()
+        predicate=triple.getPredicate().toString()
+        object=triple.getObject().toString()
         print(subject)
+
+
+
+#model.write(jpype.java.lang.System.out)
 
 jpype.shutdownJVM()
 
