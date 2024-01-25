89b73412aa08ba7f3709a2b77b9900bf4fcc4bcf
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2024-01-25 14:29:49 +0100

    Maven demo

---
+++ maven_demo/src/main/java/Example1.java
@@ -0,0 +1,25 @@
+
+import java.io.File;
+import java.net.URL;
+
+import org.apache.jena.rdf.model.Model;
+import org.linkedbuildingdata.ifc2lbd.IFCtoLBDConverter;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		URL ifcFileUrl = ClassLoader.getSystemResource("Duplex_A.ifc");
+		try {
+			File ifcFile = new File(ifcFileUrl.toURI());
+			System.out.println("Demo");
+			IFCtoLBDConverter converter = new IFCtoLBDConverter("https://example.com/", false, 1);
+			Model model = converter.convert(ifcFile.getAbsolutePath());
+			model.write(System.out, "TTL");
+			converter.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+}
