8ae9fc0f1f3da063e4ab417bef1cfbd55562a37a
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2024-08-30 10:22:54 +0200

    Code comments

--- IFCtoLBD/src/main/java/examples/Example2.java
+++ IFCtoLBD/src/main/java/examples/Example2.java
@@ -9,19 +9,22 @@ import org.linkedbuildingdata.ifc2lbd.IFCtoLBDConverter;
 public class Example2 {
 
     public static void main(String[] args) {
+        // Get the URL of the IFC file from the resources folder
         URL ifcFileUrl = ClassLoader.getSystemResource("Duplex_A.ifc");
         try {
+            // Convert the URL to a File object
             File ifcFile = new File(ifcFileUrl.toURI());
 
-            try(IFCtoLBDConverter converter = new IFCtoLBDConverter("https://example.com/", false, 1);)
-            {
-              Model model = converter.convert(ifcFile.getAbsolutePath());      
-              model.listSubjects().forEach(System.out::println);
+            // Use a try-with-resources statement to ensure the converter is closed after use
+            try(IFCtoLBDConverter converter = new IFCtoLBDConverter("https://example.com/", false, 1);) {
+                // Convert the IFC file to an RDF model
+                Model model = converter.convert(ifcFile.getAbsolutePath());
+                // List all subjects in the RDF model and print them to the standard output
+                model.listSubjects().forEachRemaining(System.out::println);
             }
 
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         }
-
     }
 }
