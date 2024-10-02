2033730cae0c5f7629d5041fa55aa75cf2272ce9
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2024-10-02 14:25:26 +0200

    Comments

--- IFCtoLBD_Desktop/src/main/java/org/linkedbuildingdata/ifc2lbd/desktop/IFCtoLBD_Desktop.java
+++ IFCtoLBD_Desktop/src/main/java/org/linkedbuildingdata/ifc2lbd/desktop/IFCtoLBD_Desktop.java
@@ -1,4 +1,3 @@
-
 /*
  *  Copyright (c) 2017 Jyrki Oraskari (Jyrki.Oraskari@gmail.f)
  * 
@@ -15,52 +14,58 @@
  * limitations under the License.
  */
 
-
 /*
- * To compile this, Java 8 is needed. jfxrt.jar is included, so, the the plugin should not be mandatory
+ * To compile this, Java 8 is needed. jfxrt.jar is included, so, the the plug-in should not be mandatory
  * but installing the http://www.eclipse.org/efxclipse/index.html and http://gluonhq.com/open-source/scene-builder/
  * make coding easier. 
- * 
  */
-
-
 
 package org.linkedbuildingdata.ifc2lbd.desktop;
 
 import org.linkedbuildingdata.ifc2lbd.application_messaging.IFC2LBD_ApplicationEventBusService;
 import org.linkedbuildingdata.ifc2lbd.application_messaging.events.IFCtoLBD_SystemExit;
 
+// Importing Google's EventBus library for event handling
 import com.google.common.eventbus.EventBus;
 
+// Importing JavaFX libraries for creating the GUI application
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/*
+ * JavaFX FXML is an XML-based language designed to build the user interface for JavaFX applications.
+ * It allows developers to define the UI layout separately from the application logic, supporting a 
+ * Model-View-Controller (MVC) architecture. This separation helps in maintaining a clean structure 
+ * for larger applications. For more details, visit: https://examples.javacodegeeks.com/java-development/desktop-java/javafx/fxml/javafx-fxml-tutorial/
+ */
 
 public class IFCtoLBD_Desktop extends Application {
+    // Initializing the event bus for handling application events
     private final EventBus eventBus = IFC2LBD_ApplicationEventBusService.getEventBus();
     
     @Override
     public void start(Stage stage) throws Exception {
+        // Loading the FXML file for the GUI layout
         Parent root = FXMLLoader.load(getClass().getResource("IFCtoLBD.fxml"));
+        // Creating a new scene with the loaded layout
         Scene scene = new Scene(root);
+        // Setting the scene to the stage and displaying it
         stage.setScene(scene);
         stage.show();
     }
 
     @Override
     public void stop() throws Exception {
+        // Posting a system exit event to the event bus when the application stops
         eventBus.post(new IFCtoLBD_SystemExit("Application exit."));
     }
 
-    
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
+        // Launching the JavaFX application
         launch(args);
     }
-    
 }
