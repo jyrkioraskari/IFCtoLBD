ea78bcf98f53f4d92fc73f8a282f081c6333e3b5
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2025-02-06 19:04:46 +0100

    Improve readability

--- IFCtoLBD/src/main/java/examples/Example2.java
+++ IFCtoLBD/src/main/java/examples/Example2.java
@@ -1,30 +1,36 @@
 package examples;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
-
 import org.apache.jena.rdf.model.Model;
 import org.linkedbuildingdata.ifc2lbd.IFCtoLBDConverter;
 
 public class Example2 {
 
+    private static final String IFC_FILE_NAME = "Duplex_A.ifc";
+    private static final String BASE_URI = "https://example.com/";
+
     public static void main(String[] args) {
-        // Get the URL of the IFC file from the resources folder
-        URL ifcFileUrl = ClassLoader.getSystemResource("Duplex_A.ifc");
         try {
-            // Convert the URL to a File object
-            File ifcFile = new File(ifcFileUrl.toURI());
-
-            // Use a try-with-resources statement to ensure the converter is closed after use
-            try(IFCtoLBDConverter converter = new IFCtoLBDConverter("https://example.com/", false, 1);) {
-                // Convert the IFC file to an RDF model
-                Model model = converter.convert(ifcFile.getAbsolutePath());
-                // List all subjects in the RDF model and print them to the standard output
-                model.listSubjects().forEachRemaining(System.out::println);
-            }
-
+            File ifcFile = getIfcFile(IFC_FILE_NAME);
+            convertIfcFile(ifcFile);
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static File getIfcFile(String fileName) throws URISyntaxException {
+        URL ifcFileUrl = ClassLoader.getSystemResource(fileName);
+        return new File(ifcFileUrl.toURI());
+    }
+
+    private static void convertIfcFile(File ifcFile) {
+        try (IFCtoLBDConverter converter = new IFCtoLBDConverter(BASE_URI, false, 1)) {
+            Model model = converter.convert(ifcFile.getAbsolutePath());
+            model.listSubjects().forEachRemaining(System.out::println);
+        } catch (Exception e) {
+            System.err.println("Conversion Error: " + e.getMessage());
         }
     }
 }
