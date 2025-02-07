5f0efbeb91e4c53c939a7ac31cab632837ce3bc6
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2025-02-07 13:47:25 +0100

    Update IFCVO.java

--- IFCtoRDF/src/main/java/com/buildingsmart/tech/ifcowl/vo/IFCVO.java
+++ IFCtoRDF/src/main/java/com/buildingsmart/tech/ifcowl/vo/IFCVO.java
@@ -23,6 +23,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+
+// IFC value object describing one line of an IFC  STEP file
+
 public class IFCVO {
 
     private Long lineNum;
