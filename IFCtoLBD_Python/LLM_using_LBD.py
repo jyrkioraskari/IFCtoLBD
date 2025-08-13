fe0451a2cf8ac786223fefe045808d240dcdcb0c
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2025-08-13 14:47:36 +0200

    Update LLM_using_LBD.py

--- IFCtoLBD_Python/LLM_using_LBD.py
+++ IFCtoLBD_Python/LLM_using_LBD.py
@@ -68,7 +68,7 @@ class GraphSparqlQAChain:
         results = self.query_graph()
         context_txt = " ".join([result["predicate"]+" "+result["object"] for result in results])
 
-        lm = dspy.LM("ollama_chat/llama3.2",
+        lm = dspy.LM("ollama_chat/deepseek-r1:8b",
             api_base="http://localhost:11434", api_key="")
         dspy.configure(lm=lm)
 
