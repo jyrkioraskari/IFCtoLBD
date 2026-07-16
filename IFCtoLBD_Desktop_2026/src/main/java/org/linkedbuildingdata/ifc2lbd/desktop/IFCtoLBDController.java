1b5377bd09c8973b6d5051e76890080dc4f9f22d
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2026-07-16 11:28:21 +0200

    Toggle button for geometry

--- IFCtoLBD_Desktop_2026/src/main/java/org/linkedbuildingdata/ifc2lbd/desktop/IFCtoLBDController.java
+++ IFCtoLBD_Desktop_2026/src/main/java/org/linkedbuildingdata/ifc2lbd/desktop/IFCtoLBDController.java
@@ -340,7 +340,7 @@ public class IFCtoLBDController implements Initializable, FxInterface {
 
 	@FXML
 	private void testGeometryWorkflow() {
-		bringFloatingCardToFront(this.geometryCard);
+		toggleFloatingCard(this.geometryCard);
 	}
 
 	@FXML
